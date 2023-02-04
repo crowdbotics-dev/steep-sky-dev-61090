6c3292944a9dd9e30249b6277b49745be2f98650
Crowdbotics <engineering@crowdbotics.com>
2023-02-04 00:10:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/steep_sky_dev_61090/MainActivity.java
@@ -0,0 +1,15 @@
+package com.steep_sky_dev_61090;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "steep_sky_dev_61090";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-sky-dev-61090",
+  url: "https://steep-sky-dev-61090.botics.co",
+  api: "https://steep-sky-dev-61090.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
